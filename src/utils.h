fcc572d6452c426e1e048836341eaaa7221ef722
kowalski <cola23g@proton.me>
2025-11-16 11:55:23 +0100

    Remove pixel_to_char function declaration

--- src/utils.h
+++ src/utils.h
@@ -6,6 +6,5 @@ struct Res { int x, y; };
 
 Res get_term_size();
 Res fit_to_term(int vid_w, int vid_h, int term_cols, int term_rows);
-char pixel_to_char(int value, std::string charset);
 void render_ascii(const cv::Mat& small, const std::string& charset);
 bool handle_url(std::string& path, std::string& temp_file);
