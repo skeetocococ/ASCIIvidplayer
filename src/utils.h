8f57ee7b661f6b888dae7bf45d9e8e66a80b1791
kowalski <cola23g@proton.me>
2025-11-20 11:45:18 +0100

    Modify render_ascii to include gamma parameter

--- src/utils.h
+++ src/utils.h
@@ -7,5 +7,5 @@ struct Res { int x, y; };
 void parse_cli(int count, char** args, long& frametime, float& gamma);
 Res get_term_size();
 Res fit_to_term(int vid_w, int vid_h, int term_cols, int term_rows);
-void render_ascii(const cv::Mat& small, const std::string& charset);
+void render_ascii(const cv::Mat& small, const std::string& charset, float gamma);
 bool handle_url(std::string& path, std::string& temp_file);
